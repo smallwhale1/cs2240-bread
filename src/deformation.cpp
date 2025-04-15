b232df77ab53cd5185da9b2d9c1ace9a4d6ab175
Sophie Zhang <sophie_zhang@brown.edu>
2025-04-15 19:35:22 -0400

    add grad calc

--- src/deformation.cpp
+++ src/deformation.cpp
@@ -27,6 +27,42 @@ void Bread::constructMockGradient() {
     }
 }
 
+std::vector<Vector3f>& Bread::calcGradient(std::vector<float> inputVec) {
+    std::vector<Vector3f> gradVector;
+    gradVector.resize(m_voxels.size());
+    for (int x = 0; x < dimX; x++) {
+        for (int y = 0; x < dimY; y++) {
+            for (int z = 0; z < dimZ; z++) {
+                int indexx1, indexx2;
+                indicesToVoxel(std::max(x - 1, 0), y, z, indexx1);
+                indicesToVoxel(std::min(x + 1, dimX - 1), y, z, indexx2);
+
+                int indexy1, indexy2;
+                indicesToVoxel(x, std::max(y - 1, 0), z, indexy1);
+                indicesToVoxel(x, std::min(y + 1, dimY - 1), z, indexy2);
+
+                int indexz1, indexz2;
+                indicesToVoxel(x, y, std::max(z - 1, 0), indexz1);
+                indicesToVoxel(x, y, std::min(z + 1, dimZ - 1), indexz2);
+
+                float gradX = (inputVec[indexx2] - inputVec[indexx1]) / 2.f;
+                float gradY = (inputVec[indexy2] - inputVec[indexy1]) / 2.f;
+                float gradZ = (inputVec[indexz2] - inputVec[indexz1]) / 2.f;
+
+                int index;
+                indicesToVoxel(x, y, z, index);
+                gradVector[index] = Vector3f(gradX, gradY, gradZ);
+            }
+        }
+    }
+
+    return gradVector;
+}
+
+std::vector<float>& Bread::gaussian(std::vector<float> inputVec) {
+
+}
+
 // for backmapping we somehow need to reverse solve for [u,v,w] in
 // [x,y,z] = [u,v,w' + pg'[u,v,w] which idk how to do rn
 void Bread::backmap(std::vector<Vector3f> grad) {
